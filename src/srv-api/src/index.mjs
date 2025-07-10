d479673a284753e4bfccca018e5c303bd26f5ed3
ast <code@ast24.dev>
2025-07-10 16:58:39 +0900

    ビルド走らないため再コミット

--- src/srv-api/src/index.mjs
+++ src/srv-api/src/index.mjs
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { AutoRouter } from 'itty-router';
 import { handler_rooms_get } from './endpoints/rooms/get.mjs';
 import { handler_crowd_get } from './endpoints/crowd/get.mjs';
@@ -16,13 +6,8 @@ import { handler_crowd_with_roomid_put } from './endpoints/crowd/with_roomid/put
 
 const router = AutoRouter();
 
-// /rooms: 教室の一覧
 router.get('/rooms', handler_rooms_get);
-
-// /crowd: 混雑状況
 router.get('/crowd', handler_crowd_get);
-
-// /crowd/:room_id: 特定教室の混雑状況
 router.get('/crowd/:room_id', handler_crowd_with_roomid_get);
 router.put('/crowd/:room_id', handler_crowd_with_roomid_put);
 
